e0d1c777fcfae3c9e1cf111bbc1ac24af43a12c2
solonkonora <nkwadanora@gmail.com>
2024-10-18 17:10:26 +0100

    done making navbar responsive

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,64 +1,6 @@
 import * as React from "react";
 import { useState } from 'react';
 
-
-// const Navigation: React.FC = () => {
-  // return (
-  //   <>
-  //     <div className="w-full flex justify-between items-center">
-  //       <div className="flex p-4">
-  //         <ul className="flex items-center ml-8">
-  //           <li>
-  //             <img
-  //               src="/images/dinosaur.png"
-  //               alt="logo"
-  //               className="w-10 h-10" 
-  //             />
-  //           </li>
-  //         </ul>
-  //       </div>
-
-  //       <div>
-  //         <ul className="flex items-center">
-  //           <li className="p-2 text-lg">
-  //             <a href="#" className="text-black">Home</a>
-  //           </li>
-  //           <li className="p-2 text-lg">
-  //             <a href="#" className="text-black">Features</a>
-  //           </li>
-  //           <li className="p-2 text-lg">
-  //             <a href="#" className="text-black">Tour-Package</a>
-  //           </li>
-  //           <li className="p-2 text-lg">
-  //             <a href="#" className="text-black">Gallery</a>
-  //           </li>
-  //           <li className="p-2 text-lg">
-  //             <a href="#" className="text-black">Bloq</a>
-  //           </li>
-  //           <li className="p-2 text-lg">
-  //             <a href="#" className="text-black">Contact-Us</a>
-  //           </li>
-  //         </ul>
-  //       </div>
-
-  //       <div>
-  //         <ul className="flex items-center">
-  //           <li>
-  //             <a
-  //               href="#"
-  //               className="bg-yellow-600 text-white border-2 border-black rounded-md py-2 px-6 mr-10 text-lg"
-  //             >
-  //               Signup
-  //             </a>
-  //           </li>
-  //         </ul>
-  //       </div>
-  //     </div>
-  //   </>
-  // );
-
-
-
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
 
--- src/components/topNav.tsx
+++ src/components/topNav.tsx
@@ -1,36 +1,36 @@
 import * as React from 'react';
 
-const MiniNavbar = () => {
-  return (
-    <nav className="flex justify-between items-center p-4 bg-teal-900">
-      <div className="flex items-center space-x-4 text-white">
-        <div className="flex items-center">
-          <img className="w-5 h-5 mr-2" src="/images/email.png" alt="Email" />
-          <span>info@example.com</span>
+  const MiniNavbar = () => {
+    return (
+      <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-teal-900">
+        <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 text-white mb-4 md:mb-0">
+          <div className="flex items-center">
+            <img className="w-4 h-4 mr-2" src="/images/email.png" alt="Email" />
+            <span>info@example.com</span>
+          </div>
+          <div className="flex items-center">
+            <img className="w-4 h-4 mr-2" src="/images/phone.png" alt="Phone" />
+            <span>237-123-456-7890</span>
+          </div>
         </div>
-        <div className="flex items-center">
-          <img className="w-5 h-5 mr-2" src="/images/phone.png" alt="Phone" />
-          <span>237-123-456-7890</span>
+  
+        {/* Right Side: Social Icons */}
+        <div className="flex flex-row md:space-x-2 space-x-2 md:space-y-0 space-y-2">
+          <div className="border border-white rounded-full p-1 md:p-2">
+            <img className="w-4 h-4 md:w-5 md:h-5" src="/images/twi.png" alt="Twitter" />
+          </div>
+          <div className="border border-white rounded-full p-1 md:p-2">
+            <img className="w-4 h-4 md:w-5 md:h-5" src="/images/fb.png" alt="Facebook" />
+          </div>
+          <div className="border border-white rounded-full p-1 md:p-2">
+            <img className="w-4 h-4 md:w-5 md:h-5" src="/images/ut.png" alt="YouTube" />
+          </div>
+          <div className="border border-white rounded-full p-1 md:p-2">
+            <img className="w-4 h-4 md:w-5 md:h-5" src="/images/linkin.png" alt="LinkedIn" />
+          </div>
         </div>
-      </div>
+      </nav>
+    );
+  };
 
-      {/* Right Side: Social Icons */}
-      <div className="flex space-x-2">
-        <div className="border border-white rounded-full p-2">
-          <img className="w-5 h-5" src="/images/twi.png" alt="Twitter" />
-        </div>
-        <div className="border border-white rounded-full p-2">
-          <img className="w-5 h-5" src="/images/fb.png" alt="facebook" />
-        </div>
-        <div className="border border-white rounded-full p-2">
-          <img className="w-5 h-5" src="/images/ut.png" alt="YouTube" />
-        </div>
-        <div className="border border-white rounded-full p-2">
-          <img className="w-5 h-5" src="/images/linkin.png" alt="Linkedin" />
-        </div>
-      </div>
-    </nav>
-  );
-};
-
-export default MiniNavbar;
+  export default MiniNavbar;
